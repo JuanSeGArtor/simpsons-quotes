2a65c05e79af8bc54f555841bd67fd545ca372fe
Juan Sebastian Gomez Ayala <j.gomezayala@globant.com>
2024-02-23 13:45:38 -0500

    agregar 'simpsons song' a home con logica e icono para mutear/desmutear

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,30 @@
 import './home.scss';
 import simpsonsImg from '../assets/theSimpsons.png';
 import { Link } from 'react-router-dom';
+import simpsonsSong from '../assets/simpsonsSong.mp3';
+import soundIcon from '../assets/soundIcon.svg';
+import { useState } from 'react';
 
 export default function Home() {
+    const [isMuted, setIsMuted] = useState(false);
+
+    const handleToggleMute = () => {
+        setIsMuted(!isMuted);
+    };
+
     return (
         <div className='home-container'>
             <img src={simpsonsImg} alt="The Simpsons" />
             <Link to='/quotes' className='start-btn special-btn'>
                 INICIAR
             </Link>
+            <button className={`mute-btn ${isMuted ? 'muted' : ''}`} onClick={handleToggleMute}>
+                <img src={soundIcon} alt="" />
+            </button>
+            <audio autoPlay muted={isMuted}>
+                <source src={simpsonsSong} type="audio/mp3" />
+                Tu navegador no soporta el elemento de audio.
+            </audio>
         </div>
     )
 }
